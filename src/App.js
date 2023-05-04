fd5c836af9e99e2650f7ae368116b8f3393abfad
BCarta7 <cannolidude@msn.com>
2023-05-04 12:50:51 -0700

    initial pages, components, and layout implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Nav';
+
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-blue-600 text-2xl text-center">Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Portfolio />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const Footer = () => {
+    return (
+        <div className="static">
+            <h1 className="w-screen text-center">Footer!</h1>
+        </div>
+    )
+}
+export default Footer
